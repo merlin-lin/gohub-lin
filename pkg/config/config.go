623ca6840fe7316ad022bbc169224ec3c2e8e363
linfeng-Mac <1371815591@qq.com>
2022-09-28 17:54:52 +0800

    发送短信（sms 包）

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,3 +1,4 @@
+// Package config 负责配置信息
 package config
 
 import (
@@ -5,37 +6,40 @@ import (
 	"os"
 
 	"github.com/spf13/cast"
-	viperlib "github.com/spf13/viper"
+	viperlib "github.com/spf13/viper" // 自定义包名，避免与内置 viper 实例冲突
 )
 
-// viper库实例
+// viper 库实例
 var viper *viperlib.Viper
 
 // ConfigFunc 动态加载配置信息
 type ConfigFunc func() map[string]interface{}
 
-// ConfigFunc先加载到此数组，loadConfig再动态生成配置
+// ConfigFuncs 先加载到此数组，loadConfig 再动态生成配置信息
 var ConfigFuncs map[string]ConfigFunc
 
 func init() {
-	// 1. 初始化Viper库
+
+	// 1. 初始化 Viper 库
 	viper = viperlib.New()
-	// 2. 配置类型
+	// 2. 配置类型，支持 "json", "toml", "yaml", "yml", "properties",
+	//             "props", "prop", "env", "dotenv"
 	viper.SetConfigType("env")
-	// 3.环境变量配置文件查找的路径，相对于main.go
+	// 3. 环境变量配置文件查找的路径，相对于 main.go
 	viper.AddConfigPath(".")
-	// 4.设置环境变量的前缀，用以区分Go的系统环境变量
+	// 4. 设置环境变量前缀，用以区分 Go 的系统环境变量
 	viper.SetEnvPrefix("appenv")
-	// 5.读取环境变量（支持flags）
+	// 5. 读取环境变量（支持 flags）
 	viper.AutomaticEnv()
 
 	ConfigFuncs = make(map[string]ConfigFunc)
 }
 
+// InitConfig 初始化配置信息，完成对环境变量以及 config 信息的加载
 func InitConfig(env string) {
-	// 1.加载环境变量
+	// 1. 加载环境变量
 	loadEnv(env)
-	// 2.注册配置信息
+	// 2. 注册配置信息
 	loadConfig()
 }
 
@@ -46,23 +50,25 @@ func loadConfig() {
 }
 
 func loadEnv(envSuffix string) {
+
+	// 默认加载 .env 文件，如果有传参 --env=name 的话，加载 .env.name 文件
 	envPath := ".env"
 	if len(envSuffix) > 0 {
 		filepath := ".env." + envSuffix
 		if _, err := os.Stat(filepath); err == nil {
+			// 如 .env.testing 或 .env.stage
 			envPath = filepath
 		}
 	}
 
-	// 加载env
+	// 加载 env
 	viper.SetConfigName(envPath)
 	if err := viper.ReadInConfig(); err != nil {
 		panic(err)
 	}
 
-	// 监控.env 变更时重新加载
+	// 监控 .env 文件，变更时重新加载
 	viper.WatchConfig()
-
 }
 
 // Env 读取环境变量，支持默认值
@@ -79,20 +85,20 @@ func Add(name string, configFn ConfigFunc) {
 }
 
 // Get 获取配置项
-// path 允许使用点式获取，如: app.name
+// 第一个参数 path 允许使用点式获取，如：app.name
+// 第二个参数允许传参默认值
 func Get(path string, defaultValue ...interface{}) string {
 	return GetString(path, defaultValue...)
 }
 
 func internalGet(path string, defaultValue ...interface{}) interface{} {
+	// config 或者环境变量不存在的情况
 	if !viper.IsSet(path) || helpers.Empty(viper.Get(path)) {
 		if len(defaultValue) > 0 {
 			return defaultValue[0]
 		}
-
 		return nil
 	}
-
 	return viper.Get(path)
 }
 
@@ -128,5 +134,5 @@ func GetBool(path string, defaultValue ...interface{}) bool {
 
 // GetStringMapString 获取结构数据
 func GetStringMapString(path string) map[string]string {
-	return cast.ToStringMapString(path)
+	return viper.GetStringMapString(path)
 }
