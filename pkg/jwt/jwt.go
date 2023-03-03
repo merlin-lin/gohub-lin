24405a15b070b0508edf97b9a8154414dd594678
linfeng-Pub <1371815591@qq.com>
2023-03-03 18:01:03 +0800

    jwt授权（未完成）

---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,41 @@
+package jwt
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrTokenExpired           error = errors.New("令牌已过期")
+	ErrTokenExpiredMaxRefresh error = errors.New("令牌已过最大刷新时间")
+	ErrTokenMalformed         error = errors.New("请求令牌格式有误")
+	ErrTokenInvalid           error = errors.New("请求令牌无效")
+	ErrHeaderEmpty            error = errors.New("需要认证才能访问！")
+	ErrHeaderMalformed        error = errors.New("请求头中 Authorization 格式有误")
+)
+
+type JWT struct {
+	// 秘钥，用以加密 JWT，读取配置信息 app.key
+	SignKey []byte
+
+	// 刷新 Token 的最大过期时间
+	MaxRefresh time.Duration
+}
+
+type JWTCustomClaims struct {
+	UserID       string `json:"user_id"`
+	UserName     string `json:"user_name"`
+	ExpireAtTime int64  `json:"expire_time"`
+
+	// StandardClaims 结构体实现了 Claims 接口继承了  Valid() 方法
+	// JWT 规定了7个官方字段，提供使用:
+	// - iss (issuer)：发布者
+	// - sub (subject)：主题
+	// - iat (Issued At)：生成签名的时间
+	// - exp (expiration time)：签名过期时间
+	// - aud (audience)：观众，相当于接受者
+	// - nbf (Not Before)：生效时间
+	// - jti (JWT ID)：编号
+
+	// todo
+}
