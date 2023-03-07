c3892a01e13ac427188f860969d4ae562e554c03
linfeng-Pub <1371815591@qq.com>
2023-03-07 17:05:47 +0800

    使用密码登录

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -111,6 +111,14 @@ func ValidationError(c *gin.Context, errors map[string][]string) {
 	})
 }
 
+// Unauthorized 响应 401，未传参 msg 时使用默认消息
+// 登录失败、jwt 解析失败时调用
+func Unauthorized(c *gin.Context, msg ...string) {
+	c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+		"message": defaultMessage("请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。", msg...),
+	})
+}
+
 // defaultMessage 内用的辅助函数，用以支持默认参数默认值
 // Go 不支持参数默认值，只能使用多变参数来实现类似效果
 func defaultMessage(defaultMsg string, msg ...string) (message string) {
