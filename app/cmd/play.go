c2d56bae33214cc6ab1539a52005d8aa0ec93f96
linfeng-Mac <1371815591@qq.com>
2023-03-15 23:09:44 +0800

    make cmd 命令

--- app/cmd/play.go
+++ app/cmd/play.go
@@ -2,21 +2,17 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
-	"gohub/pkg/console"
-	"gohub/pkg/redis"
-	"time"
 )
 
 var CmdPlay = &cobra.Command{
-	Use: "play",
+	Use:   "play",
 	Short: "Likes the Go Playground, but running at our application context",
 	Run:   runPlay,
 }
 
 func runPlay(cmd *cobra.Command, args []string) {
-	// 存进去 redis 中
-	redis.Redis.Set("hello", "hi from redis", 10*time.Second)
-	// 从 redis 里取出
-	console.Success(redis.Redis.Get("hello"))
-
+	//// 存进去 redis 中
+	//redis.Redis.Set("hello", "hi from redis", 10*time.Second)
+	//// 从 redis 里取出
+	//console.Success(redis.Redis.Get("hello"))
 }
---
+++ pkg/file/file.go
@@ -0,0 +1,21 @@
+package file
+
+import "os"
+
+// Exists 判断文件是否存在
+func Exists(filePath string) bool {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+// Put 将数据存入文件
+func Put(data []byte, to string) error {
+	err := os.WriteFile(to, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
