cc0b862c6514b46214be82985da690efcd940dab
linfeng-Mac <1371815591@qq.com>
2023-03-19 16:33:21 +0800

    migrate rollback 命令

--- pkg/migrate/migrator.go
+++ pkg/migrate/migrator.go
@@ -134,3 +134,44 @@ func (migrator *Migrator) getBatch() int {
 
 	return batch
 }
+
+// Rollback 回滚上一个操作
+func (migrator *Migrator) Rollback() {
+
+	// 获取最后一批次的迁移数据
+	lastMigration := Migration{}
+	migrator.DB.Order("id DESC").First(&lastMigration)
+	migrations := []Migration{}
+	migrator.DB.Where("batch = ?", lastMigration.Batch).Order("id DESC").Find(&migrations)
+
+	// 回滚最后一批次的迁移
+	if !migrator.rollbackMigrations(migrations) {
+		console.Success("[migrations] table is empty, nothing to rollback.")
+	}
+}
+
+func (migrator *Migrator) rollbackMigrations(migrations []Migration) bool {
+	// 标记是否真的有执行了迁移回退的操作
+	runed := false
+
+	for _, _migration := range migrations {
+		// 友好提示
+		console.Warning("rollback " + _migration.Migration)
+
+		// 执行迁移文件的 down 方法
+		mfile := getMigrationFile(_migration.Migration)
+		if mfile.Down != nil {
+			mfile.Down(database.DB.Migrator(), database.SQLDB)
+		}
+
+		runed = true
+
+		// 回退成功了就删除掉这条记录
+		migrator.DB.Delete(&_migration)
+
+		// 打印运行状态
+		console.Success("finish " + mfile.FileName)
+	}
+
+	return runed
+}
