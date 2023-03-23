3d9c7753f3ecf98c1e5d085f724608c6f19912a3
linfeng-Mac <1371815591@qq.com>
2023-03-23 23:22:57 +0800

    seed 命令

---
+++ app/cmd/seed.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"gohub/database/seeders"
+	"gohub/pkg/console"
+	"gohub/pkg/seed"
+)
+
+var CmdDBSeed = &cobra.Command{
+	Use:   "seed",
+	Short: "Insert fake data to the database",
+	Run:   runSeeders,
+	Args:  cobra.MaximumNArgs(1), // 只允许 1 个参数
+}
+
+func runSeeders(cmd *cobra.Command, args []string) {
+	seeders.Initialize()
+	if len(args) > 0 {
+		name := args[0]
+		seeder := seed.GetSeeder(name)
+		if len(seeder.Name) > 0 {
+			seed.RunSeeder(name)
+		} else {
+			console.Error("Seeder not found: " + name)
+		}
+	} else {
+		seed.RunAll()
+		console.Success("Done seeding.")
+	}
+}
